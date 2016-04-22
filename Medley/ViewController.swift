be74ee38c8eedeb66c603b5d93f104f8602df7b4
Nikolas Chaconas <nchaconas@graphiq.com>
2016-04-21 17:12:39 -0700

    added red highlighting with password confirmation

--- Medley/ViewController.swift
+++ Medley/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 extension UIViewController {
     func hideKeyboardOnTap() {
@@ -21,15 +22,22 @@ extension UIViewController {
 
 class ViewController: UIViewController {
     //Page Elements
+    @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var passwordConfirmationField: UITextField!
     @IBOutlet weak var signupButton: UIButton!
     @IBOutlet weak var loginText: UILabel!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var switchViewButton: UIButton!
-    @IBOutlet weak var HaveAccountText: UILabel!
+    @IBOutlet weak var haveAccountText: UILabel!
     var blueBackground = UIColor(red: 0, green: 128/255, blue: 1, alpha: 1.0)
     var purpleBackground = UIColor(red: 204/255, green: 102/255, blue: 1, alpha: 1.0)
+    var redColor = UIColor(red: 1, green:0, blue: 0, alpha: 0.8)
+    
+    // Create a reference to a Firebase location
+    var myRootRef = Firebase(url:"https://crackling-heat-1030.firebaseio.com/")
+    
+    
     
     //function which fades in the signup page
     func FadeInSignup() {
@@ -38,14 +46,23 @@ class ViewController: UIViewController {
             self.switchViewButton.setTitle("Log In", forState: .Normal)
             self.loginButton.alpha = 0.0
             self.loginText.text = "Sign Up"
-            self.HaveAccountText.alpha = 0.0
+            self.haveAccountText.alpha = 0.0
             self.passwordConfirmationField.alpha = 1.0
             self.emailField.alpha = 1.0
             self.signupButton.alpha = 1.0
         })
-        self.HaveAccountText.alpha = 1.0
-        self.HaveAccountText.text = "Already Have an Account?"
+        self.haveAccountText.alpha = 1.0
+        self.haveAccountText.text = "Already Have an Account?"
+       
     }
+    
+//    func ShowError(errorMessage: String, currentBackground: UIColor) {
+//        self.errorLabel.text = errorMessage
+//        self.errorLabel.textColor = self.redColor
+//        UIView.animateWithDuration(1.5, animations: {
+//            //self.errorLabel.textColor = currentBackground
+//        })
+//    }
     
     //function to fade in the login screen
     func FadeInLogin() {
@@ -59,8 +76,9 @@ class ViewController: UIViewController {
             self.loginButton.alpha = 1.0
             self.emailField.alpha = 0.0
             self.passwordConfirmationField.alpha = 0.0
-            self.HaveAccountText.text = "Don\'t Have an Account?"
+            self.haveAccountText.text = "Don\'t Have an Account?"
         })
+        
     }
     
     //locks orientation to portrait
@@ -72,7 +90,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.hideKeyboardOnTap()
-        
+        self.passwordConfirmationField.addTarget(self, action: #selector(ViewController.LoginFieldChange(_:)), forControlEvents: UIControlEvents.EditingChanged)
+        self.passwordField.addTarget(self, action: #selector(ViewController.LoginFieldChange(_:)), forControlEvents: UIControlEvents.EditingChanged)
     }
 
     override func didReceiveMemoryWarning() {
@@ -88,6 +107,26 @@ class ViewController: UIViewController {
             FadeInLogin()
         }
     }
-
+    //
+    func LoginFieldChange(sender:UITextField){
+        switch sender {
+        case passwordConfirmationField, passwordField:
+            if(passwordField.text != passwordConfirmationField.text) {
+                passwordConfirmationField.layer.borderColor = self.redColor.CGColor
+                passwordConfirmationField.layer.borderWidth = 2
+                passwordConfirmationField.layer.cornerRadius = 5
+            }
+            else {
+                passwordConfirmationField.layer.borderWidth = 0
+            }
+            
+        default:
+            break
+        }
+    }
+    
+    @IBAction func signupButtonPressed(sender: AnyObject) {
+        
+    }
 }
 
