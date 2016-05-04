c7b6552645dacef1ee947524f6c8ea97da53cbd5
Wells Zeng <NGhovat@169-231-85-134.wireless.ucsb.edu>
2016-05-03 21:15:56 -0700

    chat_bar moves up when keyboard shows

--- Medley/RoomViewController.swift
+++ Medley/RoomViewController.swift
@@ -11,11 +11,21 @@ import Firebase
 
 class RoomViewController: UIViewController {
     
+    @IBOutlet weak var album_cover: UIImageView!
+    
+    @IBOutlet weak var chat_bar: UITextField!
+    
+    @IBOutlet weak var chat_box: UIScrollView!
+    
     var myRootRef = Firebase(url:"https://crackling-heat-1030.firebaseio.com/")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(RoomViewController.keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(RoomViewController.keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
+ 
         self.hideKeyboardOnTap()
     }
     
@@ -24,5 +34,16 @@ class RoomViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
+            self.view.frame.origin.y -= keyboardSize.height
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
+            self.view.frame.origin.y += keyboardSize.height
+        }
+    }
     
 }
