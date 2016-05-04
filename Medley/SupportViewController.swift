712c7edc409cb519b294d0ca8d72a611fedf4394
Joe Song <joesong@Joes-MacBook-Pro.local>
2016-05-03 20:26:11 -0700

    Implemented e-mail sending

--- Medley/SupportViewController.swift
+++ Medley/SupportViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 import Firebase
+import MessageUI
 
-class SupportViewController: UIViewController, UITextViewDelegate {
+class SupportViewController: UIViewController, UITextViewDelegate, MFMailComposeViewControllerDelegate {
     
     var myRootRef = Firebase(url:"https://crackling-heat-1030.firebaseio.com/")
     
     @IBOutlet weak var sendButton: UIButton!
     @IBOutlet weak var messageBox: UITextView!
+    @IBOutlet weak var subjectBox: UITextField!
     
     let buttonBorderColor : UIColor = UIColor( red: 255/255.0, green: 255/255.0, blue: 255/255.0, alpha: 0.35)
     let placeholderColor : UIColor = UIColor( red: 199/255.0, green: 199/255.0, blue: 205/255.0, alpha: 1)
@@ -32,6 +34,11 @@ class SupportViewController: UIViewController, UITextViewDelegate {
         self.messageBox.layer.cornerRadius = 5
         self.messageBox.text = "Message..."
         self.messageBox.textColor = self.placeholderColor
+        self.messageBox.layer.borderWidth = 1
+        self.messageBox.layer.borderColor = self.buttonBorderColor.CGColor
+        
+        //Disable button by default
+        self.sendButton.enabled = false
         
         //Create message box listener
         messageBox.delegate = self
@@ -51,6 +58,47 @@ class SupportViewController: UIViewController, UITextViewDelegate {
             messageBox.text = "Message..."
             messageBox.textColor = self.placeholderColor
         }
+        else if (!(messageBox.text.isEmpty) && !(subjectBox.text?.isEmpty)!){
+            self.sendButton.enabled = true
+        }
+    }
+    
+    //Code from http://stackoverflow.com/questions/28963514/sending-email-with-swift
+    func configuredMailComposeViewController() -> MFMailComposeViewController{
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self
+        
+        mailComposerVC.setToRecipients(["medleymusicapp@gmail.com"])
+        mailComposerVC.setSubject(self.subjectBox.text!)
+        mailComposerVC.setMessageBody(self.messageBox.text!, isHTML: false)
+        
+        return mailComposerVC
+    }
+    
+    @IBAction func sendButtonPressed(sender: AnyObject) {
+        let mailComposeViewController = configuredMailComposeViewController()
+        if MFMailComposeViewController.canSendMail() {
+            self.presentViewController(mailComposeViewController, animated: true, completion: self.mailSent)
+        } else{
+          self.showSendMailError()
+        }
+    }
+    
+    func mailSent(){
+        self.messageBox.text = "Message..."
+        self.messageBox.textColor = self.placeholderColor
+        self.subjectBox.text = nil
+    }
+    
+    func showSendMailError(){
+        let mailErrorAlert = UIAlertController(title: "Error", message: "Your device could not send the email. Please check email configuration and try again", preferredStyle: UIAlertControllerStyle.Alert)
+        mailErrorAlert.addAction(UIAlertAction(title: "Okay", style: UIAlertActionStyle.Default , handler: nil))
+        self.presentViewController(mailErrorAlert, animated: true, completion: nil)
+    }
+    
+    func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        controller.dismissViewControllerAnimated(true, completion: nil)
+        
     }
     
     override func didReceiveMemoryWarning() {
