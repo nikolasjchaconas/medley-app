46d1e362e8d674c7ddba513ccef4e108fe1233dc
Joe Song <joesong@Joes-MacBook-Pro.local>
2016-05-27 22:45:15 -0700

    Added shadows to about page

--- Medley/AboutViewController.swift
+++ Medley/AboutViewController.swift
@@ -13,8 +13,23 @@ class AboutViewController: UIViewController {
     
     var myRootRef = Firebase(url:"https://crackling-heat-1030.firebaseio.com/")
     
+    @IBOutlet weak var aboutHeader: UILabel!
+    @IBOutlet weak var aboutMessage: UILabel!
+    
+    let buttonShadowColor : UIColor = UIColor( red: 20/255.0, green: 20/255.0, blue: 20/255.0, alpha: 1.0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Add shadow to about header message and about text
+        self.aboutHeader.layer.shadowColor = buttonShadowColor.CGColor
+        self.aboutHeader.layer.shadowOffset = CGSizeMake(0, 6)
+        self.aboutHeader.layer.shadowRadius = 3.0
+        self.aboutHeader.layer.shadowOpacity = 1.0
+        self.aboutMessage.layer.shadowColor = buttonShadowColor.CGColor
+        self.aboutMessage.layer.shadowOffset = CGSizeMake(0, 2)
+        self.aboutMessage.layer.shadowRadius = 1.0
+        self.aboutMessage.layer.shadowOpacity = 1.0
         // Do any additional setup after loading the view, typically from a nib.
         self.hideKeyboardOnTap()
     }
