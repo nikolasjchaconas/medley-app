fd90a0d1f02b8662234f209ce6ae1fb99f590191
Joe Song <joesong@Joes-MacBook-Pro.local>
2016-05-27 23:48:57 -0700

    Adjusted brightnesses of gradients and created black gradient

--- Medley/GradientLayer.swift
+++ Medley/GradientLayer.swift
@@ -11,8 +11,8 @@ import UIKit
 extension CAGradientLayer {
     
     func blueGradient() -> CAGradientLayer {
-        let blueTop = UIColor(red: 0/255.0, green: 128/255.0, blue: 255/255.0, alpha: 1.0)
-        let blueBottom = UIColor( red: 74/255.0, green: 230/255.0, blue: 230/255.0, alpha: 1.0)
+        let blueTop = UIColor(red: 0/255.0, green: 121/255.0, blue: 202/255.0, alpha: 1.0)
+        let blueBottom = UIColor( red: 62.5/255.0, green: 182.5/255.0, blue: 182.5/255.0, alpha: 1.0)
         
         let gradientColors: [CGColor] = [blueTop.CGColor, blueBottom.CGColor]
         let gradientLocations: [Float] = [0.0, 1.0]
@@ -23,9 +23,22 @@ extension CAGradientLayer {
         return gradientLayer
     }
     
+    func blackGradient() -> CAGradientLayer {
+        let grayBottom = UIColor(red: 40/255.0, green: 40/255.0, blue: 40/255.0, alpha: 1.0)
+        let blackTop = UIColor( red: 74/255.0, green: 74/255.0, blue: 74/255.0, alpha: 1.0)
+        
+        let gradientColors: [CGColor] = [blackTop.CGColor, grayBottom.CGColor]
+        let gradientLocations: [Float] = [0.0, 1.0]
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
+        gradientLayer.colors = gradientColors
+        gradientLayer.locations = gradientLocations
+        
+        return gradientLayer
+    }
+    
     func purpleGradient() -> CAGradientLayer {
-        let purpleTop = UIColor(red: 89/255.0, green: 86/255.0, blue: 214/255.0, alpha: 1.0)
-        let purpleBottom = UIColor( red:198/255.0, green: 68/255.0, blue: 252/255.0, alpha: 1.0)
+        let purpleTop = UIColor(red: 67.5/255.0, green: 64.5/255.0, blue: 160.5/255.0, alpha: 1.0)
+        let purpleBottom = UIColor( red: 148.5/255.0, green: 102/255.0, blue: 189/255.0, alpha: 1.0)
         
         let gradientColors: [CGColor] = [purpleTop.CGColor, purpleBottom.CGColor]
         let gradientLocations: [Float] = [0.0, 1.0]
