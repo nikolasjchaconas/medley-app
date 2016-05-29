ff2861b6b67edb11312cf0d5c12695593cabadd3
Joe Song <joesong@Joes-MacBook-Pro.local>
2016-05-28 17:10:50 -0700

    Changed about page to purple gradient

--- Medley/AboutViewController.swift
+++ Medley/AboutViewController.swift
@@ -18,24 +18,24 @@ class AboutViewController: UIViewController {
     
     let buttonShadowColor : UIColor = UIColor( red: 20/255.0, green: 20/255.0, blue: 20/255.0, alpha: 1.0)
     
-    let blackGrad = CAGradientLayer().blackGradient()
+    let purpleGrad = CAGradientLayer().purpleGradient()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //Add black gradient
-        self.blackGrad.frame = self.view.bounds
-        self.view.layer.addSublayer(blackGrad)
+        self.purpleGrad.frame = self.view.bounds
+        self.view.layer.addSublayer(purpleGrad)
         
         // Add shadow to about header message and about text
         self.aboutHeader.layer.shadowColor = buttonShadowColor.CGColor
         self.aboutHeader.layer.shadowOffset = CGSizeMake(0, 6)
         self.aboutHeader.layer.shadowRadius = 3.0
         self.aboutHeader.layer.shadowOpacity = 1.0
-        self.aboutMessage.layer.shadowColor = buttonShadowColor.CGColor
-        self.aboutMessage.layer.shadowOffset = CGSizeMake(0, 2)
-        self.aboutMessage.layer.shadowRadius = 1.0
-        self.aboutMessage.layer.shadowOpacity = 1.0
+//        self.aboutMessage.layer.shadowColor = buttonShadowColor.CGColor
+//        self.aboutMessage.layer.shadowOffset = CGSizeMake(0, 2)
+//        self.aboutMessage.layer.shadowRadius = 1.0
+//        self.aboutMessage.layer.shadowOpacity = 1.0
         // Do any additional setup after loading the view, typically from a nib.
         self.hideKeyboardOnTap()
     }
