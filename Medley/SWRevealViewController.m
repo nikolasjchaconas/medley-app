a73f45540949c661fa2ffc6401d43cc28b8703a7
Joe Song <joesong@Joes-MacBook-Pro.local>
2016-05-29 01:46:01 -0700

    Disabled rotation in room view

--- Medley/SWRevealViewController.m
+++ Medley/SWRevealViewController.m
@@ -764,9 +764,7 @@ const int FrontViewPositionNone = 0xff;
 
 - (NSUInteger)supportedInterfaceOrientations
 {
-    // we could have simply not implemented this, but we choose to call super to make explicit that we
-    // want the default behavior.
-    return [super supportedInterfaceOrientations];
+    return UIInterfaceOrientationPortrait | UIInterfaceOrientationPortraitUpsideDown;
 }
 
 
