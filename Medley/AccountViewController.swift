1e85376c56bddb1caaedc0f157b2bb80fbba5465
Joe Song <joesong@Joes-MacBook-Pro.local>
2016-05-28 22:12:20 -0700

    Improved password changing and verification

--- Medley/AccountViewController.swift
+++ Medley/AccountViewController.swift
@@ -12,30 +12,33 @@ class AccountViewController: UIViewController {
     
     var myRootRef = Firebase(url:"https://crackling-heat-1030.firebaseio.com/")
      
-     @IBOutlet weak var newPasswordField: UITextField!
-     @IBOutlet weak var accountHeader: UILabel!
-     @IBOutlet weak var oldPasswordField: UITextField!
-     @IBOutlet weak var temporaryPasswordWarning: UILabel!
-     @IBOutlet weak var updateInfoButton: UIButton!
-     @IBOutlet weak var successMessage: UILabel!
-     @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
-    
+    @IBOutlet weak var newPasswordField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var accountHeader: UILabel!
+    @IBOutlet weak var oldPasswordField: UITextField!
+    @IBOutlet weak var confirmPasswordField: UITextField!
+    @IBOutlet weak var temporaryPasswordWarning: UILabel!
+    @IBOutlet weak var incorrectPasswordWarning: UILabel!
+    @IBOutlet weak var updateInfoButton: UIButton!
+    @IBOutlet weak var successMessage: UILabel!
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+
     /*
      
      change these, we need to make these global somewhere
      
-     */
+    */
     var blueBackground = UIColor(red: 0, green: 128/255, blue: 1, alpha: 1.0)
     var purpleBackground = UIColor(red: 204/255, green: 102/255, blue: 1, alpha: 1.0)
-    
+
     //Color for button borders
     let buttonBorderColor : UIColor = UIColor( red: 255/255.0, green: 255/255.0, blue: 255/255.0, alpha: 0.35)
     let buttonShadowColor : UIColor = UIColor( red: 20/255.0, green: 20/255.0, blue: 20/255.0, alpha: 1.0)
     var redColor = UIColor(red: 1, green:0, blue: 0, alpha: 0.8)
     var greenColor = UIColor(red: 0, green: 1, blue: 0, alpha: 0.8)
-    
+
     let purpleGrad = CAGradientLayer().purpleGradient()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -56,76 +59,155 @@ class AccountViewController: UIViewController {
         self.accountHeader.layer.shadowRadius = 1.0
         self.accountHeader.layer.shadowOpacity = 1.0
         
-         if(self.IsPasswordTemporary()) {
+        // Add listeners to text fields
+//        self.confirmPasswordField.addTarget(self, action: #selector(ViewController.LoginFieldChange(_:)), forControlEvents: UIControlEvents.EditingChanged)
+//        self.newPasswordField.addTarget(self, action: #selector(ViewController.LoginFieldChange(_:)), forControlEvents: UIControlEvents.EditingChanged)
+//        self.oldPasswordField.addTarget(self, action: #selector(ViewController.LoginFieldChange(_:)), forControlEvents: UIControlEvents.EditingChanged)
+//        self.emailField.addTarget(self, action: #selector(ViewController.LoginFieldChange(_:)), forControlEvents: UIControlEvents.EditingDidEnd)
+        
+        if(self.IsPasswordTemporary()) {
             self.ShowWarning()
-         }
-         else {
+        }
+        else {
             self.HideWarning()
-         }
-         self.HideSuccess()
-         loadingIndicator.alpha = 0;
+        }
+        self.HideSuccess()
+        self.hideIncorrectPassword()
+        self.emailField.text = GetCurrentUserEmail(myRootRef)
+        
+        loadingIndicator.alpha = 0;
     }
      
-     override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-     }
-     
-     
-     func ShowSuccess() {
+    }
+    
+//     func AbleToSignup (sender:UITextField) {
+//         sender.layer.borderWidth = 0
+//         if(oldPasswordField == ) {
+//             if(!isRed(emailField) && !isRed(usernameField) && !isRed(passwordField) && !isRed(passwordConfirmationField)){
+//                 self.updateInfoButton.enabled = true
+//             }
+//         }
+//         else {
+//             self.updateInfoButton.enabled = false
+//         }
+//     }
+    
+    func ShowSuccess() {
         self.successMessage.text = "Password Changed!"
-     }
+    }
      
-     func HideSuccess() {
+    func HideSuccess() {
         self.successMessage.text = ""
-     }
+    }
+
+    func hideIncorrectPassword(){
+        self.incorrectPasswordWarning.text = ""
+    }
+
+    func showIncorrectPassword(){
+        self.incorrectPasswordWarning.text = "Incorrect Password"
+    }
+    
+    func showPasswordMismatch(){
+        self.incorrectPasswordWarning.text = "New passwords do not match"
+    }
      
-     func ShowLoading() {
+    func ShowLoading() {
         loadingIndicator.alpha = 1.0
         loadingIndicator.startAnimating()
-     }
+    }
      
-     func HideLoading() {
+    func HideLoading() {
         loadingIndicator.alpha = 0.0
-     }
+    }
      
-     func ShowWarning() {
+    func ShowWarning() {
         self.temporaryPasswordWarning.text = "You are using a temporary password, it will expire in the next 24 hours. Please change your password."
-     }
+    }
      
-     func HideWarning() {
+    func HideWarning() {
         self.temporaryPasswordWarning.text = ""
-     }
+    }
      
-     func IsPasswordTemporary() -> Bool {
+    func IsPasswordTemporary() -> Bool {
         return (self.GetCurrentUser(self.myRootRef).providerData["isTemporaryPassword"] as? Bool)!
-     }
+    }
      
-     func ValidField(field : UITextField) -> Bool {
+    func ValidField(field : UITextField) -> Bool {
         let valid = field.text != "" ? true : false
         if(!valid)  {
             self.MakeTextFieldRed(field, color:self.redColor)
         }
         return valid
-     }
-     
-     @IBAction func changePasswordPressed(sender: AnyObject) {
-        if(self.ValidField(self.newPasswordField) && self.ValidField(self.oldPasswordField)) {
-            ShowLoading()
-            self.myRootRef.changePasswordForUser(GetCurrentUserEmail(myRootRef), fromOld: self.oldPasswordField.text!,
-                                                 toNew: self.newPasswordField.text!, withCompletionBlock: { error in
-                                                    if error != nil {
-                                                        self.HideLoading()
-                                                    } else {
-                                                        self.HideLoading()
-                                                        self.HideWarning()
-                                                        self.ShowSuccess()
-                                                    }
-            })
+    }
+    
+    func validEmail(emailField:UITextField) -> Bool {
+        let after = emailField.text!.componentsSeparatedByString("@")
+        let num = after.count - 1
+        var separators = [""]
+        if(emailField.text == "" || num != 1) {
+            self.MakeTextFieldRed(emailField, color:self.redColor)
+            return false
         }
-        self.ShowSuccess()
-     }
-     
+        else if (after[1] == "" || after[0] == ""){
+            self.MakeTextFieldRed(emailField, color:self.redColor)
+            return false
+        }
+        else if (after[1] != "") {
+            separators = after[1].componentsSeparatedByString(".")
+        }
+        if (separators.count < 2){
+            self.MakeTextFieldRed(emailField, color:self.redColor)
+            return false
+        }
+        else if (separators[0] == "" || separators[1] == "") {
+            self.MakeTextFieldRed(emailField, color:self.redColor)
+            return false
+        }
+        else {
+            return true
+        }
+    }
+    
+    @IBAction func updateInfoPressed(sender: AnyObject) {
+        self.hideKeyboard()
+        self.HideSuccess()
+        self.ShowLoading()
+        
+        if(self.ValidField(self.oldPasswordField)){
+            if(self.newPasswordField.text != "" && self.confirmPasswordField.text != ""){
+                if(self.confirmPasswordField.text != self.newPasswordField.text){
+                    self.showPasswordMismatch()
+                }
+                else{
+                    self.myRootRef.changePasswordForUser(GetCurrentUserEmail(myRootRef), fromOld: self.oldPasswordField.text!, toNew: self.newPasswordField.text!, withCompletionBlock: { error in
+                        if error != nil{
+                            self.HideLoading()
+                            self.showIncorrectPassword()
+                        }
+                        else{
+                            self.hideIncorrectPassword()
+                            self.HideLoading()
+                            self.HideWarning()
+                            self.ShowSuccess()
+                        }
+                    })
+                }
+            }
+
+        }
+        self.HideLoading()
+    }
+    
+    override func MakeTextFieldRed(sender:UITextField, color:UIColor) {
+        sender.layer.borderColor = color.CGColor
+        sender.layer.borderWidth = 2
+        sender.layer.cornerRadius = 5
+    }
+    
     
     
 }
